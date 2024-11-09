c6559d258552b44eaf960c8a8a33a451d56064f7
Teremki16 <143702401+Teremki16@users.noreply.github.com>
2024-11-09 08:39:05 +0200

    Update at 09.11.2024

---
+++ Ярошенко/Cyberween/script.js
@@ -0,0 +1,18 @@
+fetch("https://raw.githubusercontent.com/MamboDancer/WebMiddleCyberween/main/task.json", {
+    method: "get"
+}).then(async (response)=>{
+    let data = await response.json();
+    console.log(data)
+    document.querySelector(".title").innerHTML = data.event
+
+    data.guests.forEach(el=>{
+        document.querySelector(".guests").innerHTML += `
+        <div class="gue">
+        <h1>${el.name}</h1>
+        <p>Secret identity ${el.about}</p>
+        <img src="${el.photo_url}">
+        </div>
+        `
+    })
+})
+
