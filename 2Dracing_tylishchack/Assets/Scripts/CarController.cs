0776ce94bd5877f4fa1c250e02505f4d405b2138
Teremki16 <143702401+Teremki16@users.noreply.github.com>
2024-11-30 08:38:47 +0200

    Update at 30.11.2024

--- 2Dracing_tylishchack/Assets/Scripts/CarController.cs
+++ 2Dracing_tylishchack/Assets/Scripts/CarController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Build;
 using UnityEngine;
 
 public class CarController : MonoBehaviour
@@ -7,35 +8,104 @@ public class CarController : MonoBehaviour
     [SerializeField] WheelJoint2D backWheel;
     [SerializeField] WheelJoint2D frontWheel;
     JointMotor2D motor;
+
+    private bool moveForward = false;
+    private bool moveBackward = false;
+
+    private float speed = 0f;
+
+    private bool isGrounded = false;
+
+    private Rigidbody2D rb;
+
     void Start()
     {
-        motor.maxMotorTorque = 10000;
+        motor.maxMotorTorque = 1000;
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        if(Input.GetKey(KeyCode.UpArrow))
+        if (Input.GetKey(KeyCode.UpArrow))
         {
-            motor.motorSpeed = 1000;
-            backWheel.motor = motor;
-           frontWheel.motor = motor;
-            backWheel.useMotor = true;
-           frontWheel.useMotor = true;
+            moveForward = true;
+            moveBackward = false;
         }
         else if (Input.GetKey(KeyCode.DownArrow))
         {
-            motor.motorSpeed = -1000;
-            backWheel.motor = motor;
-            frontWheel.motor = motor;
-            backWheel.useMotor = true;
-            frontWheel.useMotor = true;
+            moveForward = false;
+            moveBackward = true;
+        }
+        else
+        {
+            moveForward = false;
+            moveBackward = false;
+        }
+        if(frontWheel.GetComponent <Collider2D>(). IsTouchingLayers()||
+                backWheel .GetComponent <Collider2D>().IsTouchingLayers())
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (isGrounded)
+        {
+            if (moveForward)
+            {
+                if (frontWheel.attachedRigidbody.angularVelocity > -2000)
+                {
+                    speed += 40f;
+                    motor.motorSpeed = speed;
+                }
+                backWheel.motor = motor;
+                frontWheel.motor = motor;
+                backWheel.useMotor = true;
+                frontWheel.useMotor = true;
+            }
+            else if (moveBackward)
+            {
+                if (frontWheel.attachedRigidbody.angularVelocity < 2000)
+                {
+                    speed -= 40f;
+                    motor.motorSpeed = speed;
+                }
+                backWheel.motor = motor;
+                frontWheel.motor = motor;
+                backWheel.useMotor = true;
+                frontWheel.useMotor = true;
+            }
+            else
+            {
+                speed = -frontWheel.attachedRigidbody.angularVelocity;
+                backWheel.useMotor = false;
+                frontWheel.useMotor = false;
+            }
         }
         else
         {
             backWheel.useMotor = false;
-            frontWheel.useMotor = false;
+                frontWheel.useMotor = false;
+            if (moveForward)
+            {
+                if(rb.angularVelocity < 200)
+                {
+                    rb.AddTorque(10f);
+                }
+            }            if (moveBackward)
+            {
+                if(rb.angularVelocity > -200)
+                {
+                    rb.AddTorque(-10f);
+                }
+            }
         }
-
     }
+    
 }
