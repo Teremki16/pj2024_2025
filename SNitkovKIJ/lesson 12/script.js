80d921bf5eca1680cf1884927a71589a3395be8b
Teremki16 <143702401+Teremki16@users.noreply.github.com>
2024-12-01 09:59:25 +0300

    Update at 01.12.2024

--- SNitkovKIJ/lesson 12/script.js
+++ SNitkovKIJ/lesson 12/script.js
@@ -4,6 +4,9 @@ let isMouseDown = false;
 let canvas = document.querySelector("canvas")
 let c = canvas.getContext("2d")
 
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+
 function fillin(){
     c.fillStyle = color;
     c.fillRect(0,0, canvas.width, canvas.height)
@@ -14,5 +17,43 @@ function clearAll(){
     
 }
 
-document.querySelector("#color").addEventListener("change", ()=>color = e.target.value);
+document.querySelector("#color").addEventListener("change", (e)=>color = e.target.value);
+
+document.querySelector("#range").addEventListener("input", (e)=>{
+    lw = e.target.value
+    document.querySelector("#rangeVal").textContent = lw;
+})
+
+canvas.addEventListener("mousedown",()=>{
+    isMouseDown = true
+    c.beginPath()
+})
+
+canvas.addEventListener("mouseup",()=>(isMouseDown = false))
+
+canvas.addEventListener("mousemove", (e)=>{
+    if(isMouseDown){
+        
+    
+        c.fillStyle = color
+        c.strokeStyle = color
+        c.lineWidth = lw
+
+        c.lineTo(e.clientX, e.clientY)
+        c.stroke()
+        c.beginPath()
+        c.arc(e.clientX, e.clientY, lw/2, 0, 2 * Math.PI)
+        c.fill()
+
+        c.beginPath()
+        c.moveTo(e.clientX, e.clientY)
+    }
+})
+
+
+
+document.querySelector("#download").addEventListener("click", ()=>{
+    let url = canvas.toDataURL().replace("image/png", "image/octet-stream")
+    document.querySelector("#download").href = url
+})
     
