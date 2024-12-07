af8f6f104920b79adb1ecfb86740990fd2f0b76f
Teremki16 <teremki16robocode@gmail.com>
2024-12-07 08:31:09 +0200

    Update at 07.12.2024

---
+++ Mayorov2DRaCe/Assets/Scripts/CarController.cs
@@ -0,0 +1,158 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Build;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class CarController : MonoBehaviour
+{
+    [SerializeField] WheelJoint2D backWheel;
+    [SerializeField] WheelJoint2D frontWheel;
+    JointMotor2D motor;
+
+    private bool moveForward = false;
+    private bool moveBackward = false;
+
+    private float speed = 0f;
+
+    private bool isGrounded = false;
+
+    private Rigidbody2D rb;
+
+    private int fuel = 100;
+    [SerializeField] TextMeshProUGUI FuelText;
+
+    void Start()
+    {
+        motor.maxMotorTorque = 1000;
+        rb = GetComponent<Rigidbody2D>();
+        StartCoroutine("FuelReducer");
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            moveForward = true;
+            moveBackward = false;
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            moveForward = false;
+            moveBackward = true;
+        }
+        else
+        {
+            moveForward = false;
+            moveBackward = false;
+        }
+
+        if (frontWheel.GetComponent<Collider2D>().IsTouchingLayers() ||
+            backWheel.GetComponent<Collider2D>().IsTouchingLayers())
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+        CheckGameOver();
+    }
+
+    void FixedUpdate()
+    {
+        MoveOnGround();
+        if (!isGrounded)
+        {
+            MoveInAir();
+        }
+
+    }
+
+    private void MoveOnGround()
+    {
+        if (moveForward)
+        {
+            if (frontWheel.attachedRigidbody.angularVelocity > -2000)
+            {
+                speed += 40f;
+                motor.motorSpeed = speed;
+            }
+            backWheel.motor = motor;
+            frontWheel.motor = motor;
+            backWheel.useMotor = true;
+            frontWheel.useMotor = true;
+        }
+        else if (moveBackward)
+        {
+            if (frontWheel.attachedRigidbody.angularVelocity < 2000)
+            {
+                speed -= 40f;
+                motor.motorSpeed = speed;
+            }
+            backWheel.motor = motor;
+            frontWheel.motor = motor;
+            backWheel.useMotor = true;
+            frontWheel.useMotor = true;
+        }
+        else
+        {
+            speed = -frontWheel.attachedRigidbody.angularVelocity;
+            backWheel.useMotor = false;
+            frontWheel.useMotor = false;
+        }
+    }
+
+    private void MoveInAir()
+    {
+        backWheel.useMotor = false;
+        frontWheel.useMotor = false;
+        if (moveForward)
+        {
+            if (rb.angularVelocity < 200)
+            {
+                rb.AddTorque(10f);
+            }
+        }
+        if (moveBackward)
+        {
+            if (rb.angularVelocity > -200)
+            {
+                rb.AddTorque(-10f);
+            }
+        }
+    }
+
+    private void CheckGameOver()
+    {
+        Vector2 raydir = Vector2.up;
+        RaycastHit2D[] hit = Physics2D.RaycastAll(transform.position, raydir, 0.7f);
+        Debug.DrawRay(transform.position, raydir * 0.7f, Color.magenta);
+        if(hit.Length > 1)
+        {
+            GameOver();
+        }
+    }
+
+    private void GameOver()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+
+    IEnumerator FuelReducer()
+    {
+        while(fuel > 0)
+        {
+            fuel--;
+            FuelText.SetText(fuel.ToString());
+            yield return null new WaitForSeconds(0.5f);
+        }
+        GameOver();
+    }
+}
+
+
+
