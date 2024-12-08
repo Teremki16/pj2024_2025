0e07133d40f4c0f8267aaba1e839ba4e787ad41c
Teremki16 <143702401+Teremki16@users.noreply.github.com>
2024-12-08 08:22:05 +0200

    Update at 08.12.2024

--- nikita aboba/Flappy4JS/Flappy4JS/flappy.js
+++ nikita aboba/Flappy4JS/Flappy4JS/flappy.js
@@ -11,7 +11,7 @@ back.src = "img/back.png"
 let bird = new Image()
 bird.src = "img/bird.png"
 let pipeBotton = new Image()
-pipeBotton.src = "img/pipeBotton.png"
+pipeBotton.src = "img/pipeBottom.png"
 let pipeUp = new Image()
 pipeUp.src = "img/pipeUp.png"
 let road = new Image()
@@ -22,6 +22,8 @@ let fly = new Audio()
 fly.src = "audio/fly.mp3"
 let score = new Audio()
 score.src = "audio/score.mp3"
+let scor = new Audio()
+scor.scr = "audio/scor.mp3"
 
 let birdX = 20
 let birdY = 20
@@ -29,22 +31,66 @@ let birdY = 20
 let g = 0.5
 let velY = 0
 
-function draw(){
+let scorecount = 0
+let bestscorecount = 0
+
+let pipes = []
+pipes[0] = {
+    x: canvas.width,
+    y: 0,
+}
+function draw() {
     c.drawImage(back, 0, 0)
     c.drawImage(bird, birdX, birdY)
+    c.drawImage(road, 0, canvas.height - road.height)
 
     velY += g
     birdY += velY
 
-    if(birdY >= canvas.height){
-        location.reload()
+    for (let i = 0; i < pipes.length; i++) {
+        pipes[i].x -= 2
+        c.drawImage(pipeUp, pipes[i].x, pipes[i].y)
+        c.drawImage(pipeBotton, pipes[i].x, pipes[i].y + pipeUp.height + 130)
+
+        if (pipes[i].x == 80) {
+            pipes.push({
+                x: canvas.width,
+                y: (Math.random() * pipeUp.height) - pipeUp.height
+            })
+        }
+        if (pipes[i].x < -pipeUp.width) {
+            pipes.shift()
+            score.play()
+            scorecount++ 
+            document.querySelector(".score").innerHTML = "Score: "+scorecount
+
+
+        }
+        
+        if(birdX + bird.width >= pipes[i].x &&
+            birdX <= pipes[i].x + pipeUp.width &&
+            (
+                birdY <= pipes[i].y + pipeUp.height ||
+                birdY + bird.height >= pipes[i].y +  pipeUp.height + 130
+            )
+        ){
+        reload()
+        scor.play()
+        }
+    }
+
+
+    if (birdY >= canvas.height - road.height) {
+        reload()
+        score.play()
+        
     }
 }
 
-setInterval(draw,20)
+setInterval(draw, 20)
 
-function moveUp(){
-    if (birdX > 5){
+function moveUp() {
+    if (birdX > 5) {
         velY = -5
         fly.play()
     }
@@ -52,6 +98,20 @@ function moveUp(){
 
 canvas.addEventListener("click", moveUp)
 
-window.addEventListener("keydown", (e)=>{
-    if(e.key == "e") moveUp()
+window.addEventListener("keydown", (e) => {
+    if (e.key == "e") moveUp()
 })
+
+function reload(){
+    if(scorecount > bestscorecount) bestscorecount = scorecount
+    document.querySelector(".bestScore").innerHTML = "Best score: " + bestscorecount
+    scorecount = 0
+    birdX = 10
+    birdY = 100
+    velY = 0
+    pipes = []
+    pipes[0] = {
+        x: canvas.width,
+        y: 0
+    }
+}
