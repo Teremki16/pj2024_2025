c35466e2916b0be7838d89310bf8376312ccbb0c
Deanny-MrSuicide <yarynkagordynska2000@gmail.com>
2024-12-22 08:12:10 +0200

    Update at 22.12.2024

--- Jerma/lesson14/script.js
+++ Jerma/lesson14/script.js
@@ -11,28 +11,43 @@ let products = []
 
 let main = document.querySelector("main")
 
+let cart = (localStorage.getItem("cart_items")) || []
+
 fetch("https://basement-42c4.restdb.io/rest/products", settings)
-.then(d => d.json())
+    .then(d => d.json())
     .then(d => {
         console.log(d)
         products = d
         draw()
     })
 
-    function draw() {
-        main.innerHTML = ""
-        products.forEach(({meatgrinder, desc, photo_url, price, _id})=>{
-            let card = document.createElement("div")
-            card.className = "product"
-            card.innerHTML = `
+function draw() {
+    main.innerHTML = ""
+    products.forEach(({ meatgrinder, desc, photo_url, price, _id }) => {
+        let card = document.createElement("div")
+        card.className = "product"
+        card.innerHTML = `
               <p>${name}</p>
               <p>${desc}</p>
               <img src= "${photo_url}">
               <p>${price}$</p>
+              nclick="Add('${_id}')"<button ob>Add to card</button>
             `
-            main.appendChild(card)
+        main.appendChild(card)
 
 
-        })
+    })
 
-    }
+}
+
+function Add(id) {
+    let item = products.find(el => el.id == id)
+    cart.push(item)
+    localStorage.setItem("cart_items", JSON.stringify(cart))
+    drawCart()
+}
+
+function drawCart() {
+    let a = cart.map((el) => `<li>${el.name} | ${el.price} $</li>`).join("")
+    document.querySelector(".cart_items").innerHTML = a
+}
