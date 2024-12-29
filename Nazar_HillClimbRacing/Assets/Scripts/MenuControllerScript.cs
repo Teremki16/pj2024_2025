65246a56d07ca10cf57bcb03fc6d68d856166497
Teremki16 <143702401+Teremki16@users.noreply.github.com>
2024-12-29 09:17:08 +0200

    Update at 29.12.2024

---
+++ Nazar_HillClimbRacing/Assets/Scripts/MenuControllerScript.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuControllerScript : MonoBehaviour
+{
+    [SerializeField] GameObject[] buttons;
+    public int currentLevel = 1;
+
+    void Start()
+    {
+        currentLevel = PlayerPrefs.GetInt("CurrentLevel");
+        if(currentLevel == 0)
+        {
+            currentLevel = 1;
+            PlayerPrefs.SetInt("currentLevel", 1);
+        }
+      for(int i = 0; i < currentLevel; i++)
+        {
+            buttons[i].GetComponent<Button>().interactable = true;
+        }
+    }
+    void Update()
+    {
+        
+    }
+    public void Level(int number)
+    {
+        SceneManager.LoadScene(number);
+    }
+}
