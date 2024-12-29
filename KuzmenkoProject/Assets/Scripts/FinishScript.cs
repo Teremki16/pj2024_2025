a0ea90d9631798ff1f7578ffd95f426ecdfe4df6
Teremki16 <teremki16robocode@gmail.com>
2024-12-29 09:17:08 +0200

    Update at 29.12.2024

---
+++ KuzmenkoProject/Assets/Scripts/FinishScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FinishScript : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "wheel")
+        {
+            int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+            int nextLevel = sceneIndex + 1;
+            PlayerPrefs.SetInt("currentLevel", nextLevel);
+            SceneManager.LoadScene(nextLevel);
+        }
+    }
+}
