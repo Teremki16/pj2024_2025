23bdda8fc8e8636de8de74ca2c4b0590e452d1a6
Teremki16 <143702401+Teremki16@users.noreply.github.com>
2025-01-11 09:41:36 +0300

    Update at 11.01.2025

---
+++ Ohohoho/Assets/OTRABOTKA 14-16/Script/CarMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMovement : MonoBehaviour
+{
+    [SerializeField] private float carSpeed;
+    public bool L = false;
+    public bool R = false;
+    private void Update()
+    {
+        if(Input.GetKey(KeyCode.A) || L)
+        {
+            Move(-carSpeed);
+        }
+        else if(Input.GetKey(KeyCode.D) || R)
+        {
+            Move(carSpeed);
+        }
+    }
+    private void Move(float dir)
+    {
+        transform.Translate(dir * Time.fixedDeltaTime, 0f, 0f);
+    }
+}
