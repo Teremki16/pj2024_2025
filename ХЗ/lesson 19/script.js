6f248ba9f331023b983f03874c15ad6249e67992
Teremki16 <teremki16robocode@gmail.com>
2025-01-12 09:39:38 +0200

    Update at 12.01.2025

---
+++ ХЗ/lesson 19/script.js
@@ -0,0 +1,7 @@
+$(".box").click(function(){
+    $(this).fadeOut(2000, ()=>$(".box").fadeIn("fast"))
+})
+
+$(".head").click(function(){
+    $(this).parent().children(".body").slideToggle("fast")
+})
