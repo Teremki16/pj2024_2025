8e5dbb645702ea098ff7ca7096638ede3593a3ab
Teremki16 <143702401+Teremki16@users.noreply.github.com>
2025-01-12 09:39:37 +0200

    Update at 12.01.2025

--- honcharenko/lesson16/sscript.js
+++ honcharenko/lesson16/sscript.js
@@ -9,8 +9,16 @@ const j = require("one-liner-joke")
     a.say({text:j.getRandomJoke().body,
      e: "$",
      t: "&",
-     f: "mona-lisa"
+    F: "mona-lisa"
 
  }))
-
  )
+const path = require("path")
+ 
+ console.log(path.basename(__filename))
+ console.log(path.extname(__filename))
+ console.log(path.dirname(__filename))
+ console.log(path.parse(__filename))
+
+
+ let newPath = path.join(__dirname, "music", "DJ anonim - krutoe muzlo2000.mp3")
