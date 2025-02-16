b03612f88d8cc31760ecb0d301935639029bb24a
Teremki16 <143702401+Teremki16@users.noreply.github.com>
2025-02-16 17:08:02 +0300

    Update at 16.02.2025

---
+++ My project (2)/Assets/Script/PickUp.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PickUp : MonoBehaviour
+{
+    private Text scoreText;
+    private int score;
+    private void Awake()
+    {
+        scoreText = GameObject.Find("ScoreText").GetComponent<Text>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Heart"))
+        {
+            Destroy(collision.gameObject);
+            score++;
+            scoreText.text = $"Score: {score}";
+        }
+        else if(collision.CompareTag("Ground"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ U2D_Hrechyshkin_ISometrie/Assets/Scripts/EnemyGFX.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class EnemyGFX : MonoBehaviour
+{
+    public AIPath path;
+    public SpriteRenderer renderer;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (path.desiredVelocity.x >= 0.1f)
+        {
+            renderer.flipX = true;
+        }else if(path.desiredVelocity.x <= -0.1f)
+        {
+            renderer.flipX = false;
+        }
+    }
+
+}
