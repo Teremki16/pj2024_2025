4c58fd735723de519526c84f6da547fdd8f298ca
Teremki16 <143702401+Teremki16@users.noreply.github.com>
2025-05-10 09:15:09 +0300

    Update at 10.05.2025

---
+++ Feduk/lesson 32/script.js
@@ -0,0 +1,54 @@
+function setup(){
+    createCanvas(400, 400);
+    
+}
+
+function draw(){
+    // background("aqua")
+    // fill("")
+    // stroke("black")
+    // strokeWeight(1)
+    // circle(200, 100, 50);
+    // fill("")
+    // stroke("black")
+    // strokeWeight(1)
+    // circle(200, 150, 75);
+    // fill("")
+    // stroke("black")
+    // circle(200, 220, 100);
+    // fill("black")
+    // circle(200, 165, 5);
+    // circle(200, 150, 5);
+    // circle(200, 135, 5);
+    // circle(200, 120, 5);
+    // circle(190, 95, 5);
+    // circle(210, 95, 5);
+    // fill("red")
+    // rect(182, 45, 36, 42)
+    // fill("orange")
+    // triangle(225, 105, 200, 100, 200, 110);
+    background("aqua")
+    // fill('red')
+    // stroke("red")
+    // rect(180, 200, 55, 75)
+    // fill("aqua")
+    // rect(190, 220, 15, 15)
+    // fill("orange")
+    // stroke("orange")
+    // rect(210, 250, 15, 25)
+    // triangle(170, 200, 208, 160, 246, 200);
+    // rect(0, 275, 400, )
+    fill("yellow")
+    circle(250, 100, 50);
+    fill('black')
+    circle(240, 95, 10);
+    line(240, 122, 220, 170)
+    fill("orange")
+    triangle(300, 100, 270, 85, 270, 115);
+    fill("yellow")
+    ellipse(180, 200, 120, 80)
+    // circle(180, 200, 150);
+    line(170, 240, 150, 300)
+    line(190, 240, 220, 300)
+
+}
